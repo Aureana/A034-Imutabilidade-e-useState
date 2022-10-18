7dcb04fc1d84e5cf2750a334852772e26d713006
Aureana <111321782+Aureana@users.noreply.github.com>
2022-10-17 21:57:54 -0300

    Atividade-React-a034-Respondida

--- src/Components/PokemonCard/PokemonCard.js
+++ src/Components/PokemonCard/PokemonCard.js
@@ -3,20 +3,28 @@ import {Card, PokemonName, PokemonType, EvolveButton} from './styles'
 
 
 
-const PokemonCard = (props) => {
 
-    const evoluirPokemon = () => {
-        console.log("Cliquei no botão de evoluir")
-    }
+const PokemonCard = (props) => {
+  const novoPokemon ={
+    name: "Pichu",
+    type:"Eletric",
+    evolved: false,
+    weight:2,
+    color:'blue',
+    Image:'',
+    id:0,
+  }
+
+   
     
   return (
-    <Card color={}>
-        <img src={} alt={`Pokemon`}/>
-        <PokemonName>{}</PokemonName>
-        <PokemonType>{}</PokemonType>
-        <p>{}kg</p>
+    <Card color={props.color}>
+        <img src={props.Image} alt={`Pokemon`}/>
+        <PokemonName>{props.name}</PokemonName>
+        <PokemonType>{props.type}</PokemonType>
+        <p>{props.weight}kg</p>
 
-        <EvolveButton onClick={() => evoluirPokemon()}>Evoluir!</EvolveButton>
+        <EvolveButton onClick={props.evoluir}>Evoluir!</EvolveButton>
     </Card>
   )
 }
